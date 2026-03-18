790a3c8e9e42a1eacaa3cb0a19b5d1b8e9994c2a
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-18 09:31:46 +0530

    feat(session store module): assembled

---
+++ src/session_store/session_store.module.ts
@@ -0,0 +1,13 @@
+import {Module}                       from '@nestjs/common';
+import {LoggerModule}                 from "../logger/logger.module";
+import {NodeCacheSessionStoreService} from './node_cache.session_store/node_cache.session_store.service';
+
+
+
+@Module({
+			imports  : [LoggerModule],
+			providers: [NodeCacheSessionStoreService],
+			exports  : [NodeCacheSessionStoreService]
+		})
+export class SessionStoreModule {
+}
