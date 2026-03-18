0dea7e95cb9a083211de65109dc36db2502a744d
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-18 09:24:30 +0530

    feat(cache module): add interface

---
+++ src/cache/interface.cache.ts
@@ -0,0 +1,17 @@
+export interface ICacheService {
+	getDriver(): ICacheDriver;
+
+	getItem(key: string): unknown
+
+	setItem(key: string, buffer: string | number | Record<string, any>): unknown
+}
+
+
+
+export interface ICacheDriver {
+	get(key: string): unknown
+
+	set(key, buffer: string | number | Record<string, any>): unknown
+
+	del(key: string): unknown
+}
