acb2c03c31121be11534b6c1ebee298352bf2528
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-18 09:38:35 +0530

    feat(middleware module): updated

--- src/middleware/middleware.module.ts
+++ src/middleware/middleware.module.ts
@@ -1,18 +1,28 @@
-import { Module }       from '@nestjs/common';
-import {
-    OrganizationModule
-}                       from '../business_logic/organization/organization.module';
-import { LoggerModule } from '../logger/logger.module';
-import {
-    Middleware_OrganizationPuller
-}                       from './organization_puller.middleware';
+import {Module}                   from '@nestjs/common';
+import {OrganizationModule}       from '../business_logic/organization/organization.module';
+import {CacheModule}              from "../cache/cache.module";
+import {LoggerModule}             from '../logger/logger.module';
+import {SessionStoreModule}       from "../session_store/session_store.module";
+import {AuthMiddlewareService}    from './auth/auth.middleware.service';
+import {SessionMiddlewareService} from './session/session.middleware.service';
 
 
 
 @Module({
-            imports  : [ OrganizationModule, LoggerModule ],
-            providers: [ Middleware_OrganizationPuller ],
-            exports  : [ Middleware_OrganizationPuller ],
-        })
+			imports  : [
+				OrganizationModule,
+				LoggerModule,
+				CacheModule,
+				SessionStoreModule,
+			],
+			providers: [
+				AuthMiddlewareService,
+				SessionMiddlewareService
+			],
+			exports  : [
+				AuthMiddlewareService,
+				SessionMiddlewareService
+			],
+		})
 export class MiddlewareModule {
 }
