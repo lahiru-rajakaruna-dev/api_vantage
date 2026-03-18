95a6db630e1dc734f5cc0bdbfc6d61d40032aebf
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-18 09:31:33 +0530

    feat(session store service): add node cache session store

---
+++ src/session_store/node_cache.session_store/node_cache.session_store.service.spec.ts
@@ -0,0 +1,20 @@
+import {Test, TestingModule}          from '@nestjs/testing';
+import {NodeCacheSessionStoreService} from './node_cache.session_store.service';
+
+
+
+describe('NodeCacheSessionStoreService', () => {
+	let service: NodeCacheSessionStoreService;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+																		 providers: [NodeCacheSessionStoreService],
+																	 }).compile();
+
+		service = module.get<NodeCacheSessionStoreService>(NodeCacheSessionStoreService);
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+});
---
+++ src/session_store/node_cache.session_store/node_cache.session_store.service.ts
@@ -0,0 +1,40 @@
+import {Inject, Injectable}      from '@nestjs/common';
+import NodeCache                 from "node-cache";
+import {ICacheDriver}            from "../../cache/interface.cache";
+import type ILoggerService       from "../../logger/logger.interface";
+import {TOKEN__LOGGER_FACTORY}   from "../../logger/logger_factory/logger_factory.service";
+import {TOrganizationSelect}     from "../../orm/drizzle/drizzle-postgres/schema";
+import {BaseSessionStoreService} from "../base.session_store/base.session_store.service";
+
+
+
+@Injectable()
+export class NodeCacheSessionStoreService extends BaseSessionStoreService {
+	private readonly nodeCacheDriver: NodeCache;
+
+	constructor(@Inject(TOKEN__LOGGER_FACTORY) logger: ILoggerService) {
+		super(logger)
+		this.nodeCacheDriver = new NodeCache({
+												 stdTTL        : 60 * 60 * 24,
+												 checkperiod   : 60 * 60 * 24,
+												 useClones     : false,
+												 deleteOnExpire: true
+											 })
+	}
+
+	getDriver(): ICacheDriver {
+		return this.nodeCacheDriver;
+	}
+
+	deleteSession(id: string): boolean | undefined {
+		return super.deleteSession(id);
+	}
+
+	getSession(id: string): TOrganizationSelect | undefined {
+		return super.getSession(id);
+	}
+
+	setSession(id: string, buffer: TOrganizationSelect): boolean | undefined {
+		return super.setSession(id, buffer);
+	}
+}
