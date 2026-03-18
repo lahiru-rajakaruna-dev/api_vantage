94787a326e7b9b64608f2e9c4694bf24e9dc794c
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-18 09:30:26 +0530

    feat(session store service): add interface

---
+++ src/session_store/interface.session_store.service.ts
@@ -0,0 +1,14 @@
+import {ICacheDriver}        from "../cache/interface.cache";
+import {TOrganizationSelect} from "../orm/drizzle/drizzle-postgres/schema";
+
+
+
+export interface ISessionStore {
+	getDriver(): ICacheDriver;
+
+	getSession(id: string): TOrganizationSelect | undefined
+
+	setSession(id: string, buffer: TOrganizationSelect): boolean | undefined
+
+	deleteSession(id: string): boolean | undefined
+}
